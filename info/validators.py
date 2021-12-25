053c0dc0aca15caeeb3edd8c4fa81022de57733a
voffan <vovan_evv@rambler.ru>
2021-12-25 14:47:44 +0900

    fixed pic extension validator

--- info/validators.py
+++ info/validators.py
@@ -4,6 +4,6 @@ from django.core.exceptions import ValidationError
 
 def validate_file_extension(value):
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
-    valid_extensions = ['.jpg', '.png', '.jpeg', 'gif']
+    valid_extensions = ['.jpg', '.png', '.jpeg', '.gif']
     if ext.lower() not in valid_extensions:
         raise ValidationError('Unsupported file extension.')
